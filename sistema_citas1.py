374c9a465e8becd6a9d373ea6c462d1588a01b53
jemartinez024 <jemartinezflo@gmail.com>
2025-09-09 13:38:13 -0600

    Descripción de los cambios

--- sistema_citas1.py
+++ sistema_citas1.py
@@ -7,12 +7,10 @@ n = int(input("Ingrese la cantidad de citas a registrar: "))
 
 solicitudes = [] # lista vecia
 for i in range (1, n + 1):
-    name_solicitante = str(input("Ingrese el Nombre o los Nombres de los Solicitantes {i}: "))
-    time_cita = int(input("Ingrese el tiempo estimado de la solicitud {i}: "))
+    name_solicitante = str(input("Ingrese el Nombre o los Nombres de los Solicitantes: " ))
+    time_cita = int(input("Ingrese el tiempo estimado de la solicitud: " ))
     
 solicitudes.append(name_solicitante, time_cita) # agregamos a la lista
-
-
 print(solicitudes)
 
 
