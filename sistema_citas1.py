bbf4882bcf0aa3a99d15444eff66f3db676af990
jemartinez024 <jemartinezflo@gmail.com>
2025-09-09 14:34:54 -0600

    Descripción de los cambios

--- sistema_citas1.py
+++ sistema_citas1.py
@@ -8,10 +8,19 @@ n = int(input("Ingrese la cantidad de citas a registrar: "))
 solicitudes = [] # lista vecia
 for i in range (1, n + 1):
     name_solicitante = str(input(f"Ingrese el Nombre del Solicitante {i} : " ))
+    
+while True:
+    
     hour = int(input(f"Ingrese las horas estimadas para la solicitud {i} : "))
     min = int(input(f"Ingrese los minutos estimados para la solicitud {i} : "))
+    
     time = hour * 60 + min # # Convertir todo a minutos para poder ordenar fácilmente
-    solicitudes.append((name_solicitante, time)) # agrega una tupla a la lista
+    if time <= 120: 
+        break
+    else:
+        print("El tiempo total no debe superar los 120 minutos. Intente nuevamente.")
+        
+solicitudes.append((name_solicitante, time)) # # Guardar cada solicitud como tupla (nombre, tiempo en minutos)
 
 solicitudes.sort(key=lambda x: x[1]) # ordena la lista por el segundo elemento (tiempo de cita)
 print("Lista de solicitudes ordenadas por tiempo estimado:")
