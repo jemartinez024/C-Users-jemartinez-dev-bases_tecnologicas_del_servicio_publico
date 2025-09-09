9f6e5df76977460296e95f75f04177a1c962774e
jemartinez024 <jemartinezflo@gmail.com>
2025-09-09 15:43:04 -0600

    Descripción de los cambios

--- sistema_citas1.py
+++ sistema_citas1.py
@@ -1,26 +1,24 @@
-print("*** Bienvenido al Sistema de Citas ***")
-
-
 # Programa para registrar solicitudes con tiempo en horas y minutos
+print("*** Bienvenido al Sistema de Citas ***")
 
 n = int(input("Ingrese la cantidad de citas a registrar: "))
 
 solicitudes = [] # lista vecia
 for i in range (1, n + 1):
-    name_solicitante = str(input(f"Ingrese el Nombre del Solicitante {i} : " ))
+    name_solicitante = str(input(f"Ingrese el nombre del solicitante {i} : " ))
     
-while True:
+    while True:
     
-    hour = int(input(f"Ingrese las horas estimadas para la solicitud {i} : "))
-    min = int(input(f"Ingrese los minutos estimados para la solicitud {i} : "))
+        hour = int(input(f"Ingrese las horas estimadas para la solicitud {i} : "))
+        min = int(input(f"Ingrese los minutos estimados para la solicitud {i} : "))
     
-    time = hour * 60 + min # # Convertir todo a minutos para poder ordenar fácilmente
-    if time <= 120: 
-        break
-    else:
-        print("El tiempo total no debe superar los 120 minutos. Intente nuevamente.")
+        time = hour * 60 + min # # Convertir todo a minutos para poder ordenar fácilmente
+        if time <= 120: 
+            break
+        else:
+            print("El tiempo total no debe superar los 120 minutos. Intente nuevamente.")
         
-solicitudes.append((name_solicitante, time)) # # Guardar cada solicitud como tupla (nombre, tiempo en minutos)
+    solicitudes.append((name_solicitante, time)) # # Guardar cada solicitud como tupla (nombre, tiempo en minutos)
 
 solicitudes.sort(key=lambda x: x[1]) # ordena la lista por el segundo elemento (tiempo de cita)
 print("Lista de solicitudes ordenadas por tiempo estimado:")
@@ -28,10 +26,11 @@ print("Lista de solicitudes ordenadas por tiempo estimado:")
 for solicitud in solicitudes:
     name_solicitante = solicitud[0]
     time = solicitud[1]
-    hour = time // 60
-    min = time % 60
+    hour = time // 60 # Divide time entre 60 y devuelve solo la parte entera (sin decimales).
+    min = time % 60 # Devuelve lo que sobra después de dividir entre 60
     print(f"Nombre: {name_solicitante} - Tiempo: {hour}h {min}m")
-    #print(f"Nombre: {solicitud[0]} - Tiempo: {solicitud[1]}")
+
+print("Citas generadas exitosamente")
 
 
 
