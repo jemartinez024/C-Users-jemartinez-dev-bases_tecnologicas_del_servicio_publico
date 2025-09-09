0399ed96444c3096f44a3b5ef2fd7bd910e4235e
jemartinez024 <jemartinezflo@gmail.com>
2025-09-09 13:56:17 -0600

    Descripción de los cambios

--- sistema_citas1.py
+++ sistema_citas1.py
@@ -10,7 +10,10 @@ for i in range (1, n + 1):
     name_solicitante = str(input("Ingrese el Nombre o los Nombres de los Solicitantes: " ))
     time_cita = int(input("Ingrese el tiempo estimado de la solicitud: " ))
     
-solicitudes.append(name_solicitante, time_cita) # agregamos a la lista
+#solicitudes.append(name_solicitante, time_cita)
+solicitudes.append((name_solicitante, time_cita)) # agrega una tupla a la lista
+solicitudes.sort(key=lambda x: x[1]) # ordena la lista por el segundo elemento (tiempo de cita)
+solicitudes.sort() # ordena la lista
 print(solicitudes)
 
 
