13cfed4994a429afd3dc0e11b22f03ab5601cd9d
jemartinez024 <jemartinezflo@gmail.com>
2025-09-09 14:20:21 -0600

    Descripción de los cambios

--- sistema_citas1.py
+++ sistema_citas1.py
@@ -1,23 +1,32 @@
 print("*** Bienvenido al Sistema de Citas ***")
 
-n = 0
-i = 0
+"""n = 0
+i = 0"""
+
+# Programa para registrar solicitudes con tiempo en horas y minutos
 
 n = int(input("Ingrese la cantidad de citas a registrar: "))
 
 solicitudes = [] # lista vecia
 for i in range (1, n + 1):
     name_solicitante = str(input("Ingrese el Nombre o los Nombres de los Solicitantes: " ))
-    time_cita = int(input("Ingrese el tiempo estimado de la solicitud: " ))
-    
-#solicitudes.append(name_solicitante, time_cita)
-solicitudes.append((name_solicitante, time_cita)) # agrega una tupla a la lista
+    hour = int(input(f"Ingrese las horas estimadas para la solicitud {i}: "))
+    min = int(input(f"Ingrese los minutos estimados para la solicitud {i}: "))
+    time = hour * 60 + min # # Convertir todo a minutos para poder ordenar fácilmente
+    solicitudes.append((name_solicitante, time)) # agrega una tupla a la lista
+
+solicitudes.sort(key=lambda x: x[1]) # ordena la lista por el segundo elemento (tiempo de cita)
 print("Lista de solicitudes ordenadas por tiempo estimado:")
 
-"solicitudes.sort(key=lambda x: x[1]) # ordena la lista por el segundo elemento (tiempo de cita)"
+for solicitud in solicitudes:
+    name_solicitante = solicitud[0]
+    time = solicitud[1]
+    hour = time // 60
+    min = time % 60
+    print(f"Nombre: {name_solicitante} - Tiempo: {hour}h {min}m")
+    #print(f"Nombre: {solicitud[0]} - Tiempo: {solicitud[1]}")
 
-solicitudes.sort() # ordena la lista
-print(solicitudes)
+
 
 
 
